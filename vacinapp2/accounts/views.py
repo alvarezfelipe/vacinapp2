d2e3d49fc0b2ceb3beef8bad210e3353a11444c9
Felipe Alvarez dos Santos <alvarezfelipe_@hotmail.com>
2024-03-29 20:10:14 -0300

    tentativa de form vacina

--- vacinapp2/accounts/views.py
+++ vacinapp2/accounts/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
 
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
