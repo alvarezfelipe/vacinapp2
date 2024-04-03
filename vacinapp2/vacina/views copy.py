580cd3f27ef127fd738ca8ae82e5802309110879
Felipe Alvarez dos Santos <alvarezfelipe_@hotmail.com>
2024-04-03 15:33:22 -0300

    desisto

---
+++ vacinapp2/vacina/views copy.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Vacina
+from .forms import VacinaForm
+
+# Create your views here.
+@login_required
+def criar_vacina(request):
+    model = Vacina
+    form = VacinaForm(request.POST or None)
+
+    if str(request.method) == "POST":
+        if form.is_valid():
+            vacina = form.save()
+            messages.success(request, 'Vacina cadastrada com sucesso')
+            return redirect('listar_vacina', vacina.id)
+        else:
+            messages.error(
+                request, 'Erro ao cadastrar vacina. Consulte o administrador.'                
+            )
+    
+    print(form)
+    return render(request, 'vacina/criar_vacina.html', {'form':form})
--- vacinapp2/vacina/views.py
+++ vacinapp2/vacina/views.py
@@ -1,25 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from .forms import *
 
-from .models import Vacina
-from .forms import VacinaForm
-
-# Create your views here.
 @login_required
 def criar_vacina(request):
-    model = Vacina
-    form = VacinaForm(request.POST or None)
-
-    if str(request.method) == "POST":
-        if form.is_valid():
-            vacina = form.save()
-            messages.success(request, 'Vacina cadastrada com sucesso')
-            return redirect('listar_vacina', vacina.id)
-        else:
-            messages.error(
-                request, 'Erro ao cadastrar vacina. Consulte o administrador.'                
-            )
-    
-    print(form)
+    form = VacinaForm()
     return render(request, 'vacina/criar_vacina.html', {'form':form})
